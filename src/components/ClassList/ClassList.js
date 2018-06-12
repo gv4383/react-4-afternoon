19457461c297f5498e45585d8d3c5595a5c6c1e2
Goyo <gregory.vargas18@gmail.com>
2018-06-12 15:30:19 -0500

    completed step 5

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
     
+    this.state = {
+      students: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3005/students?class=${ this.props.match.params.class }`)
+      .then(results => {
+        this.setState({
+          students: results.data
+        });
+      });
   }
 
   render() {
+    // console.log(this.props);
+    const students = this.state.students.map((student, i) => {
+      return (
+        <h3 key={ i }>{ student.first_name } { student.last_name }</h3>
+      );
+    });
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{ this.props.match.params.class }</h1>
         <h2>ClassList:</h2>
-
+        { students }
       </div>
     )
   }
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import ClassList from './components/ClassList/ClassList'
 
 let routes = (
   <Switch>
     <Route component={ Home } exact path="/" />
     <Route component={ About } path="/about" />
+    <Route component={ ClassList } path="/classlist/:class" />
   </Switch>
 );
 
