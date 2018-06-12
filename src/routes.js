042dc229b3cd8a3d80bcf4aa4478a28f1e129429
Goyo <gregory.vargas18@gmail.com>
2018-06-12 15:42:13 -0500

    completed step 6

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
-import ClassList from './components/ClassList/ClassList'
+import ClassList from './components/ClassList/ClassList';
+import Student from './components/Student/Student';
 
 let routes = (
   <Switch>
     <Route component={ Home } exact path="/" />
     <Route component={ About } path="/about" />
     <Route component={ ClassList } path="/classlist/:class" />
+    <Route component={ Student } path="/student/:id" />
   </Switch>
 );
 
